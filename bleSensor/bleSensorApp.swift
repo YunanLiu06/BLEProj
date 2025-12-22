a963ab22c8f91b1b399e7dff3ea05bd492d5df7b
YunanLiu06 <liu4807@gmail.com>
2025-12-22 16:57:49 -0500

    Initial Commit

---
+++ bleSensor/bleSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  bleSensorApp.swift
+//  bleSensor
+//
+//  Created by Louis Lew on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct bleSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
